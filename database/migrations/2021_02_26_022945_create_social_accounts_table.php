25548e597b78494f6af8af2a74628826fdff877f
jay.sgh <huysgh94@gmail.com>
2021-02-26 09:53:54 +0700

    install laravel/socialite

--- database/migrations/2021_02_26_022945_create_social_accounts_table.php
+++ database/migrations/2021_02_26_022945_create_social_accounts_table.php
@@ -4,20 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration {
+class CreateSocialAccountsTable extends Migration {
     /**
      * Run the migrations.
      *
      * @return void
      */
     public function up() {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('social_accounts', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->nullable()->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password')->nullable();
-            $table->rememberToken();
+            $table->unsignedBigInteger('user_id');
+            $table->string('social_id');
+            $table->string('social_provider');
+            $table->string('social_name');
             $table->timestamps();
         });
     }
@@ -28,6 +27,6 @@ class CreateUsersTable extends Migration {
      * @return void
      */
     public function down() {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('social_accounts');
     }
 }
