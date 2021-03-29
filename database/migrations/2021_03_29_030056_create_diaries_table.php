0e8546fef4c8e43e087e9e12b7d3a9e6f88096b4
jay.sgh <huysgh94@gmail.com>
2021-03-29 10:16:16 +0700

    Add create_diaries_table migration

---
+++ database/migrations/2021_03_29_030056_create_diaries_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDiariesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('diaries', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 255)->nullable();
+            $table->text('content')->nullable();
+            $table->integer('word_count')->nullable();
+            $table->timestamps();
+
+            $table->foreignId('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('diaries');
+    }
+}
